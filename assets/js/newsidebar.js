d53ed5184616310bd49e1b650543308133632b37
Paulo Henrique Azevedo Rattes <128646286+PauloHenriDev@users.noreply.github.com>
2024-05-11 23:41:10 -0300

    Delete assets directory

--- assets/js/newsidebar.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.toggle');
-const sidebar = document.querySelector('.sidebar');
-
-toggleBtn.addEventListener('click', function() {
-    sidebar.classList.toggle('collapsed');
-});
